c4bf1811bdc67579d8c0c9a7bcbb132e27915c13
Nguyen Vinh Huy <johnmactavish123@gmail.com>
2023-04-04 17:32:28 +0700

    Refactor via 1.2

--- part1/src/App.js
+++ part1/src/App.js
@@ -23,21 +23,23 @@ const Header = ({course}) => {
 const Content = ({part1,part2,part3,exercises1,exercises2,exercises3}) => {
   return (
     <div>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
+      <Part name={part1} exercises={exercises1} />
+      <Part name={part2} exercises={exercises2} />
+      <Part name={part3} exercises={exercises3} />
     </div>
   )
 }
 
 const Total = ({exercises1,exercises2,exercises3}) => {
   return (<p>Number of exercises {exercises1 + exercises2 + exercises3}</p>)
+}
+
+const Part = ({name, exercises}) => {
+  return (
+    <p>
+      {name} {exercises}
+    </p>
+  )
 }
 
 export default App
