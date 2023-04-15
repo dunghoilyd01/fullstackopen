b6fb5cdf4136de0b380e23bd8bd35a6168ae4ba9
Nguyen Vinh Huy <johnmactavish123@gmail.com>
2023-04-15 16:10:11 +0700

    refactor via 1.4

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,23 +1,24 @@
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
-
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1.name} part2={part2.name} part3={part3.name} exercises1={part1.exercises} exercises2={part2.exercises} exercises3={part3.exercises} />
-      <Total exercises1={part1.exercises} exercises2={part2.exercises} exercises3={part3.exercises} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   )
 }
@@ -26,18 +27,18 @@ const Header = ({course}) => {
   return (<div>{course}</div>)
 }
 
-const Content = ({part1,part2,part3,exercises1,exercises2,exercises3}) => {
+const Content = ({parts}) => {
   return (
     <div>
-      <Part name={part1} exercises={exercises1} />
-      <Part name={part2} exercises={exercises2} />
-      <Part name={part3} exercises={exercises3} />
+      <Part name={parts[0].name} exercises={parts[0].exercises} />
+      <Part name={parts[1].name} exercises={parts[1].exercises} />
+      <Part name={parts[2].name} exercises={parts[2].exercises} />
     </div>
   )
 }
 
-const Total = ({exercises1,exercises2,exercises3}) => {
-  return (<p>Number of exercises {exercises1 + exercises2 + exercises3}</p>)
+const Total = ({parts}) => {
+  return (<p>Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>)
 }
 
 const Part = ({name, exercises}) => {
